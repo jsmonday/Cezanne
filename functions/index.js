2bc9f4bbd7c52279d80243c40ac7d1ff6a419d6a
micheleriva <master@micheleriva.it>
2019-07-13 22:36:16 +0200

    Added template validation

--- functions/index.js
+++ functions/index.js
@@ -4,14 +4,14 @@ const puppeteer = require("puppeteer");
 const functions = require('firebase-functions');
 const firebase = require("./src/firebase/storage");
 
-function uploadImage(articleId, buffer) {
+function uploadImage(articleId, imageName, buffer) {
   return new Promise((resolve, reject) => {
 
     const bufferStream = new stream.PassThrough();
     bufferStream.end(buffer);
 
     const bucket = firebase.storage().bucket();
-    const file = bucket.file(`/articles/${articleId}/cover.jpg`);
+    const file = bucket.file(`/articles/${articleId}/${imageName}.jpg`);
 
     bufferStream
       .pipe(file.createWriteStream({ metadata: { contentType: 'image/png' } }))
@@ -25,54 +25,90 @@ function uploadImage(articleId, buffer) {
   });
 }
 
-exports.createImage = functions
-  .runWith({ memory: '1GB', timeoutSeconds: 120 })
-  .https
-  .onRequest(async (req, res) => {
+function generateImage(template, data) {
 
-    const q = req.body;
+  return new Promise(async (resolve, reject) => {
+    let templateData;
 
-    if (!q.image || !q.title || !q.description || !q.articleId) {
-      res.json({
-        success: false,
-        data: "Missing required parameter: one of image, title, description, articleId"
-      })
+    switch (template) {
+      case "ogImage":
+        templateData = { file: "ogImage", screenSize: { width: 1920, height: 1080 } };
+        break;
+      case "instagramPost":
+        templateData = { file: "instagramPost", screenSize: { width: 1080, height: 1080 } };
+        break;
+      case "instagramStory":
+        templateData = { file: "instagramStory", screenSize: { width: 1080, height: 1920 } };
+        break;
     }
 
-    const render = pug.renderFile("./src/templates/ogImage.pug", {
-      title: q.title,
-      description: q.description,
+    const render = pug.renderFile(`./src/templates/${templateData.file}.pug`, {
+      title: data.title,
+      description: data.description,
+      image: data.image
     })
-      .replace("{{ image }}", q.image)
-
-    const browser = await puppeteer.launch({
-      args: [
-        '--disable-gpu',
-        '--disable-dev-shm-usage',
-        '--disable-setuid-sandbox',
-        '--no-first-run',
-        '--no-sandbox',
-        '--no-zygote',
-        '--single-process'
-      ]
-    });
-    const page = await browser.newPage();
-
-    await page.setViewport({ width: 1920, height: 1080 });
-    await page.setContent(render);
-    const renderedImage = await page.screenshot();
-    await browser.close();
 
     try {
-      const imageUrl = await uploadImage(q.articleId, renderedImage)
-      res.json({
+
+      const browser = await puppeteer.launch({
+        args: [
+          '--disable-gpu',
+          '--disable-dev-shm-usage',
+          '--disable-setuid-sandbox',
+          '--no-first-run',
+          '--no-sandbox',
+          '--no-zygote',
+          '--single-process'
+        ]
+      });
+      const page = await browser.newPage();
+
+      await page.setViewport({ ...templateData.screenSize });
+      await page.setContent(render);
+      const renderedImage = await page.screenshot();
+      await browser.close();
+
+      const imageUrl = await uploadImage(data.articleId, templateData.file, renderedImage)
+      resolve({
         success: true,
         data: imageUrl[0]
       });
+
     } catch (err) {
-      res.json({
+      reject({
         success: false,
         data: err
       });
     }
+  });
+
+}
+
+exports.createImage = functions
+  .runWith({ memory: '1GB', timeoutSeconds: 120 })
+  .https
+  .onRequest((req, res) => {
+
+    const q = req.body;
+
+    if (!q.image || !q.title || !q.description || !q.articleId || !q.template) {
+      res.json({
+        success: false,
+        data: "Missing required parameter: one of image, title, description, articleId, template"
+      })
+    }
+
+    const validTemplates = ["instagramPost", "instagramStory", "ogImage"];
+
+    if (!validTemplates.includes(q.template)) {
+      res.json({
+        success: false,
+        data: `${q.template} is not a valid template name. Must be one of: ${validTemplates.join(", ")}`
+      })
+    }
+
+    generateImage(q.template, q)
+      .then((result) => res.json(result))
+      .catch((err) => res.json(err))
+
   });
